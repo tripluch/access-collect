ed24ae7bded5e40bee8d77fa2fd18ed3bafd6cd0
synowski-jessica <synowski.jessica@hotmail.fr>
2024-11-12 13:41:35 +0100

    ajout du footer avec les pages associées

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <Navbar />
         {children}
       </body>
+      <Footer />
     </html>
   );
 }
