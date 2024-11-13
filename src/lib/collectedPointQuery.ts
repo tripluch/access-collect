9e2bf5e59a8af65037bb68f9e582276313888a2f
synowski-jessica <synowski.jessica@hotmail.fr>
2024-11-13 16:32:52 +0100

    added query to add collected point with fixed data

--- src/lib/collectedPointQuery.ts
+++ src/lib/collectedPointQuery.ts
@@ -6,3 +6,14 @@ export const getCollectedPoints = async () => {
   const selectResult = await db.select().from(collectPoint);
   return selectResult as CollectPoint[];
 };
+
+export const addCollectedPoint = async () => {
+  await db.insert(collectPoint).values({
+    name: "point test 5",
+    address: "rue domer 69006 lyon",
+    daysOfCollect: "wednesday",
+    clientId: "4dc0ad13-8d3a-4b79-b05c-9fdafa701b55",
+    organisationId: "4886b539-35a0-40c1-90eb-82622056b9f0"
+
+  })
+}
