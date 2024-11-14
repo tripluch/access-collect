d2319927bb2741b597d906c19b95163a666e74f7
ileana bolas <ileana.bolas.16@gmail.com>
2024-11-14 18:47:17 +0100

    Create layout for DashboardLinks and fix CSS

--- src/app/dashboard/components/DashboardLinks.tsx
+++ src/app/dashboard/components/DashboardLinks.tsx
@@ -24,7 +24,7 @@ const DashboardLinks = () => {
   ];
   return (
     <div
-      className="w-full h-full md:h-screen flex flex-wrap justify-center md:justify-start md:flex-col 
+      className="w-full md:w-1/3 h-full md:h-screen flex flex-wrap justify-center md:justify-start md:flex-col md:items-center 
         md:bg-transparentLightOrange"
     >
       {links.map((link) => (
@@ -32,8 +32,8 @@ const DashboardLinks = () => {
           <div
             className="w-[160px] h-[160px] mx-6 shadow-md shadow-lightOliveGreen bg-white 
                     rounded-lg my-6 md:my-4 flex flex-col justify-center items-center 
-                    md:w-[250px] md:h-[71px] 
-                    lg:w-[365px] lg:h-[71px] md:flex-row md:hover:bg-lightOrange"
+                    md:w-[200px] md:h-[60px] 
+                    lg:w-[300px] lg:h-[71px] md:flex-row md:hover:bg-lightOrange"
           >
             <Image src={link.picto} alt={link.alt} width={70} height={100} />
             <div className="text-center text-midnightBlue text-2xl md:text-xl lg:text-2xl font-sans">
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,13 +2,8 @@ import DashboardLinks from "./components/DashboardLinks";
 
 export default async function Dashboard() {
   return (
-    <div className="flex">
-      <div>
-        <DashboardLinks />
-      </div>
-      <div className="hidden md:block">
-        <h1 className="text-midnightBlue">{"Bonjour Super Admin,"}</h1>
-      </div>
+    <div className="hidden md:block">
+      <h1 className="text-midnightBlue">{"Bonjour Super Admin,"}</h1>
     </div>
   );
 }
