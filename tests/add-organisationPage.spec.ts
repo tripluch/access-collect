71a0c2f9934be14f4b392f9794a0ce49d86bcba2
ileana bolas <ileana.bolas.16@gmail.com>
2024-12-06 10:07:21 +0100

    remove comments on test file

--- tests/add-organisationPage.spec.ts
+++ tests/add-organisationPage.spec.ts
@@ -30,28 +30,28 @@ test.describe("Add Organisation Page", () => {
   });
 
   test("OrgaForm should add a new organisation to db", async ({ page }) => {
-    // await page
-    //   .getByPlaceholder("OrganisationExemple")
-    //   .fill("Organisation Playwright");
-    // await page
-    //   .getByPlaceholder("1 rue de l'exemple 01234 ExempleVille")
-    //   .fill("2 rue du test 01111 TestVille");
-    // await page.getByPlaceholder("0123456789").fill("0101020203");
-    // await page.getByPlaceholder("Luc Exemple").fill("Jean-Paul Playwrîght");
-    // await page.getByPlaceholder("AGR-2023-0567").fill("AGR-2345-0567");
-    // await expect(page.getByPlaceholder("OrganisationExemple")).toHaveValue(
-    //   /[a-zA-Z0-9À-ÖØ-öø-ÿ -]/,
-    // );
-    // await expect(
-    //   page.getByPlaceholder("1 rue de l'exemple 01234 ExempleVille"),
-    // ).toHaveValue(/[a-zA-Z0-9À-ÖØ-öø-ÿ -]/);
-    // await expect(page.getByPlaceholder("0123456789")).toHaveValue(/[0-9]/);
-    // await expect(page.getByPlaceholder("Luc Exemple")).toHaveValue(
-    //   /[a-zA-Z-À-ÖØ-öø-ÿ -]/,
-    // );
-    // await expect(page.getByPlaceholder("AGR-2023-0567")).toHaveValue(
-    //   /^[a-zA-Z0-9\-]+$/,
-    // );
+    await page
+      .getByPlaceholder("OrganisationExemple")
+      .fill("Organisation Playwright");
+    await page
+      .getByPlaceholder("1 rue de l'exemple 01234 ExempleVille")
+      .fill("2 rue du test 01111 TestVille");
+    await page.getByPlaceholder("0123456789").fill("0101020203");
+    await page.getByPlaceholder("Luc Exemple").fill("Jean-Paul Playwrîght");
+    await page.getByPlaceholder("AGR-2023-0567").fill("AGR-2345-0567");
+    await expect(page.getByPlaceholder("OrganisationExemple")).toHaveValue(
+      /[a-zA-Z0-9À-ÖØ-öø-ÿ -]/,
+    );
+    await expect(
+      page.getByPlaceholder("1 rue de l'exemple 01234 ExempleVille"),
+    ).toHaveValue(/[a-zA-Z0-9À-ÖØ-öø-ÿ -]/);
+    await expect(page.getByPlaceholder("0123456789")).toHaveValue(/[0-9]/);
+    await expect(page.getByPlaceholder("Luc Exemple")).toHaveValue(
+      /[a-zA-Z-À-ÖØ-öø-ÿ -]/,
+    );
+    await expect(page.getByPlaceholder("AGR-2023-0567")).toHaveValue(
+      /^[a-zA-Z0-9\-]+$/,
+    );
     const requestPromise = page.waitForRequest("dashboard/add-organisation");
     await page.getByRole("button", { name: "Confirmer" }).click();
     await requestPromise;
