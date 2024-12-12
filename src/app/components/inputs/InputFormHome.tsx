4afbe7213d68124d90602e88ab59900b335b2400
ileana bolas <ileana.bolas.16@gmail.com>
2024-12-12 16:03:49 +0100

    refacto of inputs

---
+++ src/app/components/inputs/InputFormHome.tsx
@@ -0,0 +1,32 @@
+export const InputFormHome = ({
+    name,
+    label,
+    placeholder,
+  }: {
+    name: string;
+    label: string;
+    placeholder: string;
+  }) => {
+    return (
+      <>
+
+<div className="flex flex-col">
+        <label
+          htmlFor={name}
+          className="text-midnightBlue text-base"
+        >
+          {label}
+          
+        </label>
+        <input
+          className="p-2 bg-transparentLightOrange text-midnightBlue text-sm rounded-md  w-72 h-8 md:w-96"
+          name={name}
+          type="text"
+          placeholder={placeholder}
+          required
+        />
+      </div>
+      </>
+    )
+}
+
