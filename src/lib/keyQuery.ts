667fc25265ff18592d31c611c47205d1ff46a6cc
ileana bolas <ileana.bolas.16@gmail.com>
2024-12-16 15:12:46 +0100

    create keyQuery file and refactor sendResetPasswordEmail...

---
+++ src/lib/keyQuery.ts
@@ -0,0 +1,26 @@
+import { revalidatePath } from "next/cache";
+import { db } from "./drizzle";
+import { Key, key } from "./schema/key";
+
+export const addKey = async(userId : any) => {
+     try {
+        await db
+          .insert(key)
+          .values({
+            userId: userId,
+          })
+          .returning();
+    
+        revalidatePath("/forgotten-password");
+      } catch (error) {
+        console.error(error);
+      }
+};
+
+export const getKeyByUserId = async(idUser : any) => {
+    const key = await db.query.key.findFirst({
+        where: (key, { eq }) => eq(key.userId, idUser),
+      })
+    return key as Key;
+    
+}
