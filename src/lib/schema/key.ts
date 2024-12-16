b2e4dd7f8fe80be82162c160ca4fc85d91587589
ileana bolas <ileana.bolas.16@gmail.com>
2024-12-16 11:01:01 +0100

    change path of reset-password and create key schema

---
+++ src/lib/schema/key.ts
@@ -0,0 +1,23 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { user } from "./user";
+import { InferInsertModel, InferSelectModel, relations } from "drizzle-orm";
+
+export const key = pgTable("key", {
+    id: text("id")
+        .primaryKey()
+        .$defaultFn(() => crypto.randomUUID()),
+    userId: text("userId")
+        .references(() => user.id, { onDelete: "cascade" })
+        .notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
+
+export const keysRelations = relations(key, ({ one }) => ({
+    user: one(user, {
+        fields: [key.userId],
+        references: [user.id],
+      }),
+}));
+
+export type NewKey = InferInsertModel<typeof key>;
+export type Key = InferSelectModel<typeof key>;
