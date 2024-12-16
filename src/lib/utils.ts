5f203714ac7c442b553efc990acd664afbe135da
ileana bolas <ileana.bolas.16@gmail.com>
2024-12-16 15:12:46 +0100

    create keyQuery file and refactor sendResetPasswordEmail...

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import nodemailer from "nodemailer";
+
 export const replaceEmptyValueByNull = async (formData: FormData) => {
   const inputData = Object.fromEntries(formData);
 
@@ -11,3 +13,38 @@ export const replaceEmptyValueByNull = async (formData: FormData) => {
   });
   return newData;
 };
+
+export const creationOfTransporter = async () => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    service: process.env.MAIL_SERVICE,
+    port: Number(process.env.MAIL_PORT),
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+    tls: { rejectedUnauthorized: false },
+  });
+  return transporter;
+};
+
+export const sendMailToUser = async (url: string, info: any) => {
+  let mailBodyHtml: string = '\
+      <h3>Bonjour, </h3> \
+      <p>Vous recevez ce mail car nous avons reçu une demande de réinitialisation du mot de passe pour votre compte.<br>\
+      Veuillez cliquer sur ce lien : <a href="'+ url + '">Réinitialiser mon mot de passe</a></p>';
+  try {
+    info.sendMail({
+      from: '"access-collect" <contact@tripluch.fr>',
+      to: "ileana.bolas.16@gmail.com",
+      subject: "Réinitialisation de votre mot de passe",
+      html: mailBodyHtml,
+    });
+    console.log("Message sent to user");
+  } catch {
+    console.error("Message not sent to user");
+  }
+
+
+}
