1a1e785f18fd000dae184b9ab93e506a3378ff1e
root <root@Jess.>
2024-12-17 11:36:39 +0100

    presentation homepage desktop

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Image from "next/image";
+import LogButton from "../components/button/logButton";
 
 export const metadata: Metadata = {
   title: "Access Collect",
@@ -10,28 +11,38 @@ export default function Home() {
   return (
     <section className="text-midnightBlue">
       <div className="pt-8 mb-4 h-52">
-        <p className="mx-4 text-xl font-section">
+        <Image
+          className="absolute top-[136px] left-5 hidden md:block"
+          src="/decoration.svg"
+          alt={"élément décoratif"}
+          width={70}
+          height={70}
+        />        
+        <p className="mx-4 text-xl font-section md:w-2/3 md:text-4xl md:ml-16 md:mr-0 md:mt-12">
           {"Optimisez la collecte des déchets en entreprise avec une solution"}
           <span className="text-lightOrange">{" inclusive"}</span> {"et"}{" "}
           <span className="text-lightOrange">{"écologique"}</span>
         </p>
       </div>
+      <div className="hidden md:block md:ml-16 md:mt-16">
+        <LogButton label={"Demander une démo"} />
+      </div>
+        <Image
+          className="absolute top-64 left-16 md:bg-transparentImperialYellow md:top-40 md:rounded-full md:p-4 md:left-[880px] md:w-[400px] md:h-[400px]"
+          src="/containeurs.svg"
+          alt={"poubelle"}
+          width={250}
+          height={100}
+        />
       <Image
-        className="absolute top-64 left-16"
-        src="/containeurs.svg"
-        alt={"poubelle"}
-        width={250}
-        height={100}
-      />
-      <Image
-        className="absolute top-[286px] left-0"
+        className="absolute top-[286px] left-0 block md:hidden"
         src="/decoration.svg"
         alt={"poubelle"}
         width={70}
         height={70}
       />
-      <div className="bg-transparentImperialYellow  py-4 h-52">
-        <p className="mx-4 pt-12">
+      <div className="bg-transparentImperialYellow py-4 h-52 md:bg-white ">
+        <p className="mx-4 pt-12  md:w-2/3 md:text-xl md:ml-16 md:mr-0">
           {
             "Une application pour faciliter la collecte des déchets recyclables en entreprise, tout en favorisant l’inclusion des personnes handicapées."
           }
--- src/app/components/button/logButton.tsx
+++ src/app/components/button/logButton.tsx
@@ -1,4 +1,4 @@
-const logButton = ({ label }: { label: string }) => {
+const logButton = ({ label}: { label: string }) => {
   return (
     <>
       <button className="bg-lightOrange lg:hover:bg-white md:hover:bg-white hover:bg-lightOrange text-midnightBlue rounded p-2 font-title">
