5e0d0779f9da13d4890fb09b492bd0ce243c7e33
IleanaBls <ileana.bolas.16@gmail.com>
2025-03-07 18:34:50 +0100

    feat - add redirection to forgotten password pages

--- src/lib/keyQuery.ts
+++ src/lib/keyQuery.ts
@@ -1,29 +1,54 @@
 import { revalidatePath } from "next/cache";
 import { db } from "./drizzle";
 import { Key, key } from "./schema/key";
+import { eq } from "drizzle-orm";
 
 
 export const addKey = async (userId: any) => {
-  const date = new Date();
-  date.setHours(date.getHours()+1)
+
+  // const date = (new Date()).getTimezoneOffset() * 60000;
+  // const localeDate = (new Date(Date.now() - date));
+  // const expirationDate = new Date(localeDate.setHours((localeDate.getHours()+2)))
+
+  const date = new Date()
+  const expirationDate = new Date(date.setHours((date.getHours()+2)))
+
+
   try {
     await db
       .insert(key)
       .values({
         userId: userId,
-        expirationDate: date,
+        expirationDate: expirationDate,
       })
       .returning();
-
+    
     revalidatePath("/forgotten-password");
   } catch (error) {
     console.error(error);
   }
 };
 
-export const getKeyByUserId = async (idUser: any) => {
+export const getKeyByUserId = async (idUser: string) => {
   const key = await db.query.key.findFirst({
     where: (key, { eq }) => eq(key.userId, idUser),
   });
   return key as Key;
 };
+
+export const deleteKey = async(userKey: string) => {
+  const deletedKey = await db.delete(key).where(eq(key.id, userKey)).returning({deletedId : key.id})
+  if(!deletedKey){
+    return {error : "Key not deleted"}
+  }
+  return {result : "Key deleted"}
+}
+
+
+
+
+
+
+
+
+
