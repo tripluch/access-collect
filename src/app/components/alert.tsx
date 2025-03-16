f153498809c508f94af4a63d2fcb9aed6a9676e3
IleanaBls <ileana.bolas.16@gmail.com>
2025-03-16 14:28:19 +0100

    run format fix

--- src/app/components/alert.tsx
+++ src/app/components/alert.tsx
@@ -1,53 +1,53 @@
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
-export const successAlert = async(text: string) => {
-    const result = await Swal.fire({
-        text: text,
-        position: 'top',
-        color: "#7F9F80",
-        showClass: {
-            popup: `
+export const successAlert = async (text: string) => {
+  const result = await Swal.fire({
+    text: text,
+    position: "top",
+    color: "#7F9F80",
+    showClass: {
+      popup: `
             animate__animated
             animate__fadeInDown
             animate__faster
           `,
-          },
-        hideClass: {
-            popup: `
+    },
+    hideClass: {
+      popup: `
             animate__animated
             animate__fadeOutUp
             animate__faster
           `,
-          },
-          grow: 'row',
-          showConfirmButton: false,
-          showCloseButton: true,
-    })
-    return result
-}
+    },
+    grow: "row",
+    showConfirmButton: false,
+    showCloseButton: true,
+  });
+  return result;
+};
 
-export const errorAlert = async(text: string) => {
+export const errorAlert = async (text: string) => {
   const result = await Swal.fire({
-      text: text,
-      position: 'top',
-      color: "#FFA632",
-      showClass: {
-          popup: `
+    text: text,
+    position: "top",
+    color: "#FFA632",
+    showClass: {
+      popup: `
           animate__animated
           animate__fadeInDown
           animate__faster
         `,
-        },
-      hideClass: {
-          popup: `
+    },
+    hideClass: {
+      popup: `
           animate__animated
           animate__fadeOutUp
           animate__faster
         `,
-        },
-        grow: 'row',
-        showConfirmButton: false,
-        showCloseButton: true,
-  })
-  return result
-}
+    },
+    grow: "row",
+    showConfirmButton: false,
+    showCloseButton: true,
+  });
+  return result;
+};
