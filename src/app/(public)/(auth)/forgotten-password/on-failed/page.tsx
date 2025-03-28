ab036d901faa9686244c4ad05d600ef619a5fba6
IleanaBls <ileana.bolas.16@gmail.com>
2025-03-28 14:19:06 +0100

    fix - margin top on on-failed and on-token-expired pages

--- src/app/(public)/(auth)/forgotten-password/on-failed/page.tsx
+++ src/app/(public)/(auth)/forgotten-password/on-failed/page.tsx
@@ -3,14 +3,12 @@ import OrangeButton from "@/app/components/button/orangeButton";
 
 const ForgottenPasswordError = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="w-[300px] h-[300px] md:w-[450px] md:h-[350px] shadow-md shadow-lightOliveGreen rounded-lg flex flex-col justify-center items-center gap-4 ">
-        <h1 className="text-brightOrange uppercase">{"Oups.. "}</h1>
-        <p className="text-midnightBlue text-center px-6 py-4">
-          {"Il semble qu'un problème soit survenu."}
-        </p>
-        <OrangeButton label={"ACCUEIL"} route={"/"} />
-      </div>
+    <div className="flex flex-col items-center justify-center mt-56">
+      <h1 className="text-brightOrange uppercase">{"Oups.. "}</h1>
+      <p className="text-midnightBlue text-center px-6 py-4">
+        {"Il semble qu'un problème soit survenu."}
+      </p>
+      <OrangeButton label={"ACCUEIL"} route={"/"} />
     </div>
   );
 };
