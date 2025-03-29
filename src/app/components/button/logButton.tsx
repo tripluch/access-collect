5fe645e8bec14a84346f5b2efbf53eeef165f494
synowski-jessica <synowski.jessica@hotmail.fr>
2025-03-29 15:15:54 +0100

    add style to button orange

--- src/app/components/button/logButton.tsx
+++ src/app/components/button/logButton.tsx
@@ -1,9 +1,21 @@
-const logButton = ({ label }: { label: string }) => {
+const logButton = ({ label, route, padding }: { label: string, route: string, padding: number }) => {
+
+  const paddingClasses: Record<number, string> = {
+    1: "px-1 py-1",
+    2: "px-2 py-2",
+    3: "px-3 py-3",
+    4: "px-4 py-4",
+    5: "px-5 py-5",
+  };
+
   return (
     <>
-      <button className="bg-lightOrange lg:hover:bg-brightOrange md:hover:bg-white hover:bg-lightOrange text-midnightBlue rounded p-2 font-title text-normal lg:text-2xl lg:px-10 lg:py-5">
-        {label}
-      </button>
+      <a
+        href={route}
+        className={`c-btn c-btn--orange w-84 font-title text-normal ${paddingClasses[padding] || "px-4 py-4"} md:text-3xl`}
+        aria-label={`Cliquer sur le bouton ${label}`} >
+        <span>{label}</span>
+      </a >
     </>
   );
 };
